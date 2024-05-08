1c19971ffd9d1d88cdbf9dde1c1bbf4a44095de3
nicksui <772844248@qq.com>
2024-05-09 02:59:17 +0800

    snowflakeId

--- topgun-cbs/src/main/java/priv/nick/cbs/topgun/domain/base/BaseEntity.java
+++ topgun-cbs/src/main/java/priv/nick/cbs/topgun/domain/base/BaseEntity.java
@@ -2,12 +2,29 @@ package priv.nick.cbs.topgun.domain.base;
 
 import lombok.Data;
 
+import java.time.Instant;
+
 /**
  * @since 2024-4-19 21:30
  * @author nicksy
  */
 @Data
 public class BaseEntity {
+    /**
+     * snowflakeId
+     */
     private Long id;
 
+    /**
+     * Optimistic lock /乐观锁
+     */
+    private int version;
+
+    private Instant createdAt;
+
+    private Instant updatedAt;
+
+    private String createdBy;
+
+    private String updatedBy;
 }
